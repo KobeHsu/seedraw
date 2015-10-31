4df4c3618b81fd4afe349eaa4891f0ab49491a64
Kobe Hsu <nekokobe@gmail.com>
2015-11-01 02:50:24 +0800

    improve move function add midpoint

--- js/see_draw_path.js
+++ js/see_draw_path.js
@@ -4,7 +4,7 @@ function getElementXYofConn(bBox, elName) {
 
     if ("close" == elName) {
         xy.push(bBox.x2);
-        xy.push(bBox.y2 - 3*CIRCLE_R);
+        xy.push(bBox.y2 - 3 * CIRCLE_R);
     }
 
     return xy;
@@ -23,6 +23,7 @@ function addConnector() {
     newConn.mouseover(connectorMouseOver);
     newConn.mouseout(connectorMouseOut);
     newConn.mousedown(connectorMouseDown);
+    newConn.node.addEventListener("contextmenu", connectorContextMenu);
 
     var len = newConn.getTotalLength();
 
@@ -45,6 +46,7 @@ function addConnector() {
 
     startPoint.mouseover(connectorMouseOver);
     startPoint.mouseout(connectorMouseOut);
+    startPoint.mousedown(midPointMouseDown);
 
     pointId = grp + "point_2";
     var end = Snap.path.getPointAtLength(newConn, len);
@@ -64,10 +66,22 @@ function addConnector() {
     close.addClass("hide");
     close.attr("id", closeId);
 
-    //var grpId = grp + "g";
-    //var g = gSvg.g(newConn, midPoint, startPoint, endPoint);
-    //g.attr("id", grpId);
+    var grpId = grp + "g";
+    var g = gSvg.g(newConn, midPoint, startPoint, endPoint);
+    g.attr("id", grpId);
 
+    gSerialNo++;
+}
+
+function connectorContextMenu(e) {
+
+    e.preventDefault();
+
+    var grp = getGroupPrefix(this.id);
+    var grpId = grp + "g";
+    gSvg.select("#" + grpId).remove();
+
+    return false;
 }
 
 function connectorMouseOver() {
@@ -106,16 +120,18 @@ function connectorMouseDown(event) {
 
     conn.addClass("toFront");
 
-    correctConnectorXY(conn);
+    //correctConnectorXY(grp, conn);
 
     gDrawArea.onmousemove = connectorMouseMove;
     gDrawArea.onmouseup = connectorMouseUp;
 
 }
 
-function correctConnectorXY(conn) {
+function correctConnectorXY(grp, conn) {
 
-    var tStrAry = Snap.parseTransformString(conn.attr("transform"));
+    var g = gSvg.select("#" + grp + "g");
+
+    var tStrAry = Snap.parseTransformString(g.attr("transform"));
 
     if (tStrAry.length == 0) {
         return;
@@ -123,6 +139,8 @@ function correctConnectorXY(conn) {
 
     var x = parseInt(tStrAry[0][1], 10);
     var y = parseInt(tStrAry[0][2], 10);
+
+    g.attr("transform", "");
 
     var pathStr = conn.attr("d");
     var pathAry = Snap.parsePathString(pathStr);
@@ -141,6 +159,14 @@ function correctConnectorXY(conn) {
 
     conn.attr("transform", "");
     conn.attr("d", newPath);
+
+    gSvg.selectAll("[id^='" + grp + "point'").forEach(function (element) {
+        var cx = parseInt(element.attr("cx"), 10);
+        var cy = parseInt(element.attr("cy"), 10);
+        element.attr("transform", "");
+        element.attr("cx", cx + x);
+        element.attr("cy", cy + y);
+    });
 
 }
 
@@ -164,15 +190,136 @@ function connectorMouseMove() {
     var myMatrix = new Snap.Matrix();
     myMatrix.translate(dx, dy);
 
-    conn.transform(myMatrix);
+    var grpId = grp + "g";
+    gSvg.select("#" + grpId).transform(myMatrix);
+    //conn.transform(myMatrix);
 
 }
 
 function connectorMouseUp() {
 
     if ("" != gCurrGrp) {
+
+        var grp = getGroupPrefix(gCurrGrp);
         var conn = gSvg.select("#" + gCurrGrp + "connector");
+
+        correctConnectorXY(grp, conn);
         conn.removeClass("toFront");
+
+    }
+
+    gDrawArea.onmousemove = null;
+    gDrawArea.onmouseup = null;
+
+    gCurrGrp = "";
+
+}
+
+function midPointMouseDown(event) {
+
+    gCurrGrp = this.attr("id");
+
+    var midPoint = gSvg.select("#" + gCurrGrp);
+
+    midPoint.data("mousedown-x", event.clientX);
+    midPoint.data("mousedown-y", event.clientY);
+
+    var grp = getGroupPrefix(gCurrGrp);
+    var conn = gSvg.select("#" + grp + "connector");
+    conn.unmouseout(connectorMouseOut);
+    midPoint.unmouseout(connectorMouseOut);
+    midPoint.addClass("toFront");
+
+    gDrawArea.onmousemove = midPointMouseMove;
+    gDrawArea.onmouseup = midPointMouseUp;
+
+}
+
+function midPointMouseMove() {
+
+    if ("" == gCurrGrp) {
+        return;
+    }
+
+    var midPoint = gSvg.select("#" + gCurrGrp);
+
+    var x = event.clientX - gStartX;
+    var y = event.clientY - gStartY;
+
+    midPoint.attr("cx", x);
+    midPoint.attr("cy", y);
+
+    //x = (parseInt(midPoint.data('mousedown-x')) || 0);
+    //y = (parseInt(midPoint.data('mousedown-y')) || 0);
+    //
+    //var dx = event.clientX - x;
+    //var dy = event.clientY - y;
+    //
+    var idx = parseInt(gCurrGrp.substr(gCurrGrp.lastIndexOf(SEPARATOR) + 1), 10);
+    //
+    //var myMatrix = new Snap.Matrix();
+    //myMatrix.translate(dx, dy);
+    //
+    //midPoint.transform(myMatrix);
+    //
+    var grp = getGroupPrefix(gCurrGrp);
+    var conn = gSvg.select("#" + grp + "connector");
+    var pathStr = conn.attr("d");
+    var pathAry = Snap.parsePathString(pathStr);
+
+    //var cx = parseInt(midPoint.attr("cx"), 10);
+    //var cy = parseInt(midPoint.attr("cy"), 10);
+    pathAry[idx][1] = x;
+    pathAry[idx][2] = y;
+
+    var newPath = "";
+    pathAry.forEach(function (p) {
+        newPath += p[0] + " ";
+        newPath += p[1] + " ";
+        newPath += p[2] + " ";
+    });
+
+    conn.attr("d", newPath);
+    //
+    //var virtualPointId = gCurrGrp.substr(0, gCurrGrp.lastIndexOf(SEPARATOR) + 1) + "" + (idx + 1);
+    //var virtualPoint = gSvg.select("#" + virtualPointId);
+    //
+    //var totalPaths = (gSvg.selectAll("[id^='" + grp + "point'").length - 1) / 2;
+    //
+    //
+    //var mid = Snap.path.getPointAtLength(conn, conn.getTotalLength() / totalPaths / 2);
+    //virtualPoint.attr("cx", mid.x);
+    //virtualPoint.attr("cy", mid.y);
+
+}
+
+function midPointMouseUp() {
+
+    if ("" != gCurrGrp) {
+
+        var midPoint = gSvg.select("#" + gCurrGrp);
+        midPoint.removeClass("toFront");
+
+        var grp = getGroupPrefix(gCurrGrp);
+        var conn = gSvg.select("#" + grp + "connector");
+        conn.mouseout(connectorMouseOut);
+        midPoint.mouseout(connectorMouseOut);
+
+        //var tStrAry = Snap.parseTransformString(midPoint.attr("transform"));
+        //
+        //if (tStrAry.length != 0) {
+        //
+        //    var x = parseInt(tStrAry[0][1], 10);
+        //    var y = parseInt(tStrAry[0][2], 10);
+        //
+        //    var cx = parseInt(midPoint.attr("cx"), 10);
+        //    var cy = parseInt(midPoint.attr("cy"), 10);
+        //    midPoint.attr("transform", "");
+        //    midPoint.attr("cx", cx + x);
+        //    midPoint.attr("cy", cy + y);
+        //
+        //}
+
     }
 
     gDrawArea.onmousemove = null;
