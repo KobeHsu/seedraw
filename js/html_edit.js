a9ce821f11daa6d8c176312821713d13734765da
Kobe Hsu <nekokobe@gmail.com>
2015-11-26 15:31:59 +0800

    basic elements: text, list, label, textarea, button, radio, checkbox

--- js/html_edit.js
+++ js/html_edit.js
@@ -27,7 +27,10 @@ function dropDown(ev) {
     var elType = ev.dataTransfer.getData("elType");
     var elId = ev.dataTransfer.getData("elId");
 
-    console.log(elType + ":" + ev.clientX);
+    //console.log(ev.clientX + ":" + ev.clientY);
+
+    const ADJ_X = 50;
+    const ADJ_Y = 20;
 
     if ("span" == elType) {
 
@@ -38,8 +41,8 @@ function dropDown(ev) {
 
         span.innerHTML = "LABEL";
         span.style.position = "absolute";
-        span.style.left = ev.clientX - 50 + "px";
-        span.style.top = ev.clientY - 30 + "px";
+        span.style.left = ev.clientX - ADJ_X + "px";
+        span.style.top = ev.clientY - ADJ_Y + "px";
 
         span.addEventListener("dragstart", function () {
             dragStart(event, 'move');
@@ -63,14 +66,37 @@ function dropDown(ev) {
 
         text.innerHTML = "LABEL";
         text.style.position = "absolute";
-        text.style.left = ev.clientX - 50 + "px";
-        text.style.top = ev.clientY - 30 + "px";
+        text.style.left = ev.clientX - ADJ_X + "px";
+        text.style.top = ev.clientY - ADJ_Y + "px";
 
         text.addEventListener("dragstart", function () {
             dragStart(event, 'move');
         });
 
         gContainer.appendChild(text);
+
+        gSerialNo++;
+
+    } else if ("list" == elType) {
+
+        var list = document.createElement("input");
+
+        list.setAttribute("list", "newList");
+        if (gInitDisable) {
+            list.setAttribute("disabled", "disabled");
+        }
+        list.setAttribute("draggable", "true");
+        list.setAttribute("id", elType + "_" + gSerialNo);
+
+        list.style.position = "absolute";
+        list.style.left = ev.clientX - ADJ_X + "px";
+        list.style.top = ev.clientY - ADJ_Y + "px";
+
+        list.addEventListener("dragstart", function () {
+            dragStart(event, 'move');
+        });
+
+        gContainer.appendChild(list);
 
         gSerialNo++;
 
@@ -85,14 +111,34 @@ function dropDown(ev) {
 
         button.innerHTML = "LABEL";
         button.style.position = "absolute";
-        button.style.left = ev.clientX - 50 + "px";
-        button.style.top = ev.clientY - 30 + "px";
+        button.style.left = ev.clientX - ADJ_X + "px";
+        button.style.top = ev.clientY - ADJ_Y + "px";
 
         button.addEventListener("dragstart", function () {
             dragStart(event, 'move');
         });
 
         gContainer.appendChild(button);
+
+        gSerialNo++;
+
+    } else if ("radio" == elType || "checkbox" == elType) {
+
+        var radio = document.createElement("input");
+
+        radio.setAttribute("type", elType);
+        radio.setAttribute("draggable", "true");
+        radio.setAttribute("id", elType + "_" + gSerialNo);
+
+        radio.style.position = "absolute";
+        radio.style.left = ev.clientX + "px";
+        radio.style.top = ev.clientY + "px";
+
+        radio.addEventListener("dragstart", function () {
+            dragStart(event, 'move');
+        });
+
+        gContainer.appendChild(radio);
 
         gSerialNo++;
 
@@ -108,8 +154,8 @@ function dropDown(ev) {
         textarea.setAttribute("id", elType + "_" + gSerialNo);
 
         textarea.style.position = "absolute";
-        textarea.style.left = ev.clientX - 50 + "px";
-        textarea.style.top = ev.clientY - 30 + "px";
+        textarea.style.left = ev.clientX - ADJ_X + "px";
+        textarea.style.top = ev.clientY - ADJ_Y + "px";
 
         textarea.addEventListener("dragstart", function () {
             dragStart(event, 'move');
@@ -121,8 +167,13 @@ function dropDown(ev) {
 
     } else if ("move" == elType) {
         var el = document.getElementById(elId);
-        el.style.left = ev.clientX - 50 + "px";
-        el.style.top = ev.clientY - 30 + "px";
+        if (el.getAttribute("type") != "radio" && el.getAttribute("type") != "checkbox") {
+            el.style.left = ev.clientX - ADJ_X + "px";
+            el.style.top = ev.clientY - ADJ_Y + "px";
+        } else {
+            el.style.left = ev.clientX + "px";
+            el.style.top = ev.clientY + "px";
+        }
     }
 
 }
